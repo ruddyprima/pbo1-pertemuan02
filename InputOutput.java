bdde2470177ec4fe21a55ab04c4631564fc9f622
ruddyprima <62361121+ruddyprima@users.noreply.github.com>
2020-03-23 20:14:46 -0700

    Add files via upload

---
+++ InputOutput.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class InputOutput{
+ public static void main(String [] args){
+  Scanner scanner = new Scanner(System.in);
+  String nama;
+  System.out.print("masukan nama anda: ");
+  nama = scanner.nextLine();
+  System.out.println("Hai, "+nama);
+ }
+}
